32ebcb54854c3ee18192e4833d8ab4cd275d6ffc
NgYuHong322 <147986561+NgYuHong322@users.noreply.github.com>
2024-06-17 20:25:04 +0800

    pausemenu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,16 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+/*
+ * Author: Ng Yu Hong
+ * Date: 16/6/24
+ * Decription: Main Menu
+ */
 
 public class MainMenu : MonoBehaviour
 {
    public void PlayGame()
     {
+       // change scene to game when click play
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
     public void QuitGame()
     {
+        // quit game 
         Application.Quit(); 
     }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
+/*
+ * Author: NgYuHong
+ * Date: 17/6/24
+ * Description: Player script
+ */
+
+public class Player : MonoBehaviour
+{
+    private void Update()
+    {
+    }
+
+}
